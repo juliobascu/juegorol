576d5dc1631b587ad042dcd794f767924f29834a
juliobascu <juliorobertoandres@gmail.com>
2023-06-29 17:13:18 -0400

    Pagina GM doble columna y CSS

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'abc.123'
+app.config['MYSQL_PASSWORD'] = 'root'
 app.config['MYSQL_DB'] = 'juegorol'
 
 mysql = MySQL(app)
