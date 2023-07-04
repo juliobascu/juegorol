8f63b91cc6124e9111082cf9ce92a146d8f95b2e
juliobascu <juliorobertoandres@gmail.com>
2023-07-04 08:41:47 -0400

    modales y demas apurate edu

--- static/js/GM.js
+++ static/js/GM.js
@@ -1,16 +1,5 @@
-$(document).ready(function() {
-    $('.tarjeta').click(function() {
-        
-        var nombrePersonaje = $(this).find('.item:nth-child(1)').text();
-        var razaPersonaje = $(this).find('.item:nth-child(2)').text();
-        var nivelPersonaje = $(this).find('.item:nth-child(3)').text();
-
-        
-        $('#nombrePersonaje').val(nombrePersonaje);
-        $('#razaPersonaje').val(razaPersonaje);
-        $('#nivelPersonaje').val(nivelPersonaje);
-
-        
-        $('#formularioModal').modal('show');
-    });
-});
+function obtenerValores() {
+    var id = document.getElementById("nombrepj");
+    var idtxt = id.textContent;
+    console.log(idtxt);
+};
